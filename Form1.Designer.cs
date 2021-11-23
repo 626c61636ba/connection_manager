efabe26845321a04e8bd2e869b2448d42604d693
Victor Ângelo <victorangelograca@hotmail.com>
2021-11-23 10:32:24 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace connection_manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.commando = new System.Windows.Forms.CheckBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // commando
+            // 
+            this.commando.AutoSize = true;
+            this.commando.Location = new System.Drawing.Point(167, 309);
+            this.commando.Name = "commando";
+            this.commando.Size = new System.Drawing.Size(107, 17);
+            this.commando.TabIndex = 1;
+            this.commando.Text = "commando mode";
+            this.commando.UseVisualStyleBackColor = true;
+            this.commando.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(437, 291);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 338);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.commando);
+            this.Name = "Form1";
+            this.Text = "connection manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckBox commando;
+        private System.Windows.Forms.ListView listView1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace connection_manager
+{
+    public partial class Form1 : Form
+    {
+        private bool setup = false;
+        public string netAdapter = "Ethernet";
+
+        public Form1()
+        {
+            InitializeComponent();
+            setAdapters();
+
+            listView1.ItemChecked += new ItemCheckedEventHandler(checked_item);
+        }
+
+        private void setAdapters()
+        {
+            Process p = new Process();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = "netsh.exe";
+            p.StartInfo.Arguments = "interface show interface";
+            p.Start();
+
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+
+            string[] lines = output.Split('\n');
+
+            listView1.BeginUpdate();
+
+            for (int i=3; i < lines.Length; i++)
+            {
+                if (!String.IsNullOrEmpty(lines[i]) && (!lines[i].StartsWith("\n") && !lines[i].StartsWith("\r")))
+                {
+                    int j = 16;
+                    string adapterName = "";
+                    string[] adapter = lines[i].Split(' ');
+                    bool check = false;
+
+
+                    if (adapter[0].Contains("Enabled"))
+                        check = true;
+
+
+                    for (; j< adapter.Length; j++)
+                    {
+                        adapterName += adapter[j] + " ";
+                    }
+
+                    listView1.Items.Add(new ListViewItem(adapterName.Trim())
+                    {
+                        Checked = check
+                    });
+                }
+            }
+
+            listView1.EndUpdate();
+        }
+
+        private void checked_item(object sender, ItemCheckedEventArgs e)
+        {
+            if (setup)
+            {
+                string action;
+                if (e.Item.Checked)
+                    action = "enabled";
+                else
+                    action = "disabled";
+
+                Process.Start("netsh.exe", $"interface set interface \"{e.Item.Text}\" {action}");
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                netAdapter = listView1.SelectedItems[0].Text;
+            }
+
+            if (!backgroundWorker1.IsBusy)
+                backgroundWorker1.RunWorkerAsync();
+            else
+                backgroundWorker1.CancelAsync();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            setup = true;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            InterceptKeys.InitializeComponent(netAdapter);
+        }
+    }
+}
